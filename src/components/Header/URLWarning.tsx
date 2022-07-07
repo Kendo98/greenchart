f1916c2aaaede2bb84ab50c7553507777d4756c8
Ad-Market <78874562+Ad-Market@users.noreply.github.com>
2022-07-07 22:22:41 +0200

    Update URLWarning.tsx

--- src/components/Header/URLWarning.tsx
+++ src/components/Header/URLWarning.tsx
@@ -34,7 +34,7 @@ export default function URLWarning() {
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
     </PhishAlert>
-  ) : window.location.hostname === 'liquidity.lz.finance' ? (
+  ) : window.location.hostname === 'mhd-phi.vercel.app' ? (
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={12} /> Always make sure the URL is
