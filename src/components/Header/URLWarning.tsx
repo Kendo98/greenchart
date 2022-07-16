ea197a558127100793ba9d2a2728cfa9b81aab2a
Ad-Market <78874562+Ad-Market@users.noreply.github.com>
2022-07-16 22:40:55 +0200

    Update URLWarning.tsx

--- src/components/Header/URLWarning.tsx
+++ src/components/Header/URLWarning.tsx
@@ -30,15 +30,15 @@ export default function URLWarning() {
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={12} /> Make sure the URL is
-        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>https://mhd-phi.vercel.app</code>
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>https://swap.mhd.community</code>
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
     </PhishAlert>
-  ) : window.location.hostname === 'mhd-phi.vercel.app' ? (
+  ) : window.location.hostname === 'swap.mhd.community' ? (
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={12} /> Always make sure the URL is
-        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>https://mhd-phi.vercel.app</code> -
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>https://swap.mhd.community/</code> -
         bookmark it to be safe.
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
